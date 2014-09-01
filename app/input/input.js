12e92496654cc350bb33ee1ccffcb74a062876c8
Carlos Manuel <manuel@codecery.com>
2014-09-01 18:38:17 -0500

    include Parse

--- app/input/input.js
+++ app/input/input.js
@@ -1,6 +1,5 @@
  angular.module('myApp.controllers')
     .controller('InputController', ['$scope', function($scope) {
-      $scope.nombre = "Carlos Manuel";
       
       $scope.asuntos = [
        {id: 1, name: 'Problema'},
@@ -21,4 +20,26 @@
        {id: 3, name: 'Otro'},
       ];
 
-    }]);
+
+      $scope.saveTickets = function(){
+        Parse.initialize("6MOaLqbspioqwFGiWa7Zn7OXgfh7wa1lMYcji8mz", "5vY2KiRA8sW0FEKuaDvzDIeikDVX8lAKqAgiZwg1");
+        var Ticket = Parse.Object.extend("Ticket");
+        var ticket = new Ticket();
+   
+        ticket.set("nombre", ticket_params.nombre);
+        ticket.set("apellido", ticket.apellido);
+        ticket.set("email", ticket.email);
+   
+        ticket.save(null, {
+        success: function(ticket) {
+        // Execute any logic that should take place after the object is saved.
+        alert('New object created with objectId: ' + ticket.id);
+        },
+        error: function(ticket, error) {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and description.
+        alert('Failed to create new object, with error code: ' + error.message);
+        }
+        });
+      }
+}]);
---
+++ app/ticket/ticket.js
@@ -0,0 +1,20 @@
+
+Parse.initialize("6MOaLqbspioqwFGiWa7Zn7OXgfh7wa1lMYcji8mz", "5vY2KiRA8sW0FEKuaDvzDIeikDVX8lAKqAgiZwg1");
+var Ticket = Parse.Object.extend("Ticket");
+var ticket = new Ticket();
+ 
+ticket.set("nombre", ticket.nombre);
+ticket.set("apellido", ticket.apellido);
+ticket.set("email", ticket.email);
+ 
+ticket.save(null, {
+  success: function(ticket) {
+    // Execute any logic that should take place after the object is saved.
+    alert('New object created with objectId: ' + ticket.id);
+  },
+  error: function(ticket, error) {
+    // Execute any logic that should take place if the save fails.
+    // error is a Parse.Error with an error code and description.
+    alert('Failed to create new object, with error code: ' + error.message);
+  }
+});
